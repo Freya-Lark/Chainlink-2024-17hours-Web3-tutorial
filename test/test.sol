5acd703d8ce60a725152c1818e89fb71c1eb4a8f
Freya-Lark <seven200829@gmail.com>
2025-07-27 14:51:02 +0800

    function 可以修改变量的值

--- test/test.sol
+++ test/test.sol
@@ -2,9 +2,19 @@
 pragma solidity ^0.8.20;
 
 contract HelloWorld{
-    string strVal = "Hello World";
+    string strVar = "Hello World";
     function sayHello() public view returns (string memory) {
-        return strVal;
-
+        return strVar;
     }
+
+    function setHelloWorld(string memory newString) public{
+        strVar = newString;
+    }
+
+    //function可以修改变量的值
+
+
+ 
+
+
 }
